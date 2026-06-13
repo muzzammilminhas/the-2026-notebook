cf5bb3b8d254ada1cad7fde35ea677d781e2accc
muzzammilminhas <muzzammilminhas5@gmail.com>
2026-06-13 19:21:32 +0500

    Add installable PWA support

---
+++ public/sw.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = 'the-2026-notebook-v1'
+const APP_ROOT = new URL('./', self.registration.scope).pathname
+const APP_SHELL = [
+  APP_ROOT,
+  `${APP_ROOT}manifest.webmanifest`,
+  `${APP_ROOT}icons/app-icon.svg`,
+  `${APP_ROOT}icons/pwa-192.png`,
+  `${APP_ROOT}icons/pwa-512.png`,
+  `${APP_ROOT}icons/pwa-maskable-512.png`,
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting()),
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key)),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request
+  if (request.method !== 'GET') return
+
+  const url = new URL(request.url)
+  if (url.origin !== self.location.origin) return
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(APP_ROOT, copy))
+          return response
+        })
+        .catch(() => caches.match(APP_ROOT)),
+    )
+    return
+  }
+
+  event.respondWith(
+    caches.match(request).then(
+      (cached) =>
+        cached ||
+        fetch(request).then((response) => {
+          if (response.ok) {
+            const copy = response.clone()
+            caches.open(CACHE_NAME).then((cache) => cache.put(request, copy))
+          }
+          return response
+        }),
+    ),
+  )
+})
